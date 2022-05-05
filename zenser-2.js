f3d59be87f550cab13c074333090484a012c24b2
Amit Nayek <amitnk19@gmail.com>
2022-05-05 12:42:06 +0530

    Initial Commit

---
+++ zenser-2.js
@@ -0,0 +1,24 @@
+import react, { useContext } from "Ract";
+
+
+const store = {
+    show: false
+};
+
+let conaText = useContext(reduser);
+
+let reduser = useReduser(store)
+
+conaText.producser = () => {
+
+useEffect(() => {
+axios.fetch().then(res => {
+})
+}
+
+ return  { store , dispatch };
+}
+
+
+return (    
+conaText.conSumer
