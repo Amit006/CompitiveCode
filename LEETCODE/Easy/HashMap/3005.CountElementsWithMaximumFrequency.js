166eaa06a75e18d454872715206fcaaa31ec9159
Amit <amitnk19@gmail.com>
2026-08-18 18:06:47 +0530

    Add implementation for Count Elements with Maximum Frequency problem

---
+++ LEETCODE/Easy/HashMap/3005.CountElementsWithMaximumFrequency.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxFrequencyElements = function (nums) {
+    const map = new Map(); let maximum = 0, sum = 0;
+    for (const x of nums) {
+        const freq = map.get(x) || 0
+        map.set(x, freq + 1)
+        maximum = maximum < (freq + 1) ? freq + 1 : maximum;
+    }
+    const arr = [...map.values()]
+    for (let i = 0; i < arr.length; i++) if (arr[i] == maximum) sum += arr[i];
+    return sum;
+};
+
+console.log(maxFrequencyElements([1, 2, 2, 3, 3, 3])); // Output: 3
+console.log(maxFrequencyElements([1, 1, 2, 2, 2, 3])); // Output: 3
