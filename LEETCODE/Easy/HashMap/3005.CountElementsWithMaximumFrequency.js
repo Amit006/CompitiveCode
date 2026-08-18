2f1e63e2d1ab93efa0e6c0cbe23bcc9ff0be493f
Amit <amitnk19@gmail.com>
2026-08-18 18:07:30 +0530

    Fix formatting issues in maxFrequencyElements function

--- LEETCODE/Easy/HashMap/3005.CountElementsWithMaximumFrequency.js
+++ LEETCODE/Easy/HashMap/3005.CountElementsWithMaximumFrequency.js
@@ -5,11 +5,11 @@
 var maxFrequencyElements = function (nums) {
     const map = new Map(); let maximum = 0, sum = 0;
     for (const x of nums) {
-        const freq = map.get(x) || 0
-        map.set(x, freq + 1)
+        const freq = map.get(x) || 0;
+        map.set(x, freq + 1);
         maximum = maximum < (freq + 1) ? freq + 1 : maximum;
     }
-    const arr = [...map.values()]
+    const arr = [...map.values()];
     for (let i = 0; i < arr.length; i++) if (arr[i] == maximum) sum += arr[i];
     return sum;
 };
