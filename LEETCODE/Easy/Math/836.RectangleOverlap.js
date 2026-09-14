5d5ed407e00e7bcdcaaf7bb19f978649424908bd
Amit <amitnk19@gmail.com>
2026-09-15 03:58:42 +0530

    Add implementation for Rectangle Overlap problem

---
+++ LEETCODE/Easy/Math/836.RectangleOverlap.js
@@ -0,0 +1,16 @@
+
+/**
+ * @param {number[]} rec1
+ * @param {number[]} rec2
+ * @return {boolean}
+ */
+var isRectangleOverlap = function (rec1, rec2) {
+
+    return !(rec1[2] <= rec2[0] ||
+        rec2[2] <= rec1[0] ||
+        rec1[3] <= rec2[1] ||
+        rec2[3] <= rec1[1])
+};
+
+console.log(isRectangleOverlap([0, 0, 2, 2], [1, 1, 3, 3])) // true
+console.log(isRectangleOverlap([0, 0, 1, 1], [1, 0, 2, 1])) // false
